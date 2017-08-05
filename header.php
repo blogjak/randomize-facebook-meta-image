766f33f57a4d879cc0980c8919c90744f7463667
Blogjak <bezimeni.id@gmail.com>
2017-08-05 16:32:46 +0700

    Update and rename files.php to header.php

--- header.php
+++ header.php
@@ -1,16 +1,33 @@
 <?php
+
+// Randomize images
  $metaimg = array("images/1.jpg",
               "images/2.jpg",
               "images/3.jpg");
 $img = $metaimg[array_rand($metaimg)];
 
+// Randomize description texts
 $metadescription = array("Text description 1",
 "Text description 2",
 "Text description 3");
 $description = $metadescription[array_rand($metadescription)];
 
+// Randomize title text
 $metatitle = array("Text title 1",
 "Text title 2",
 "Text title 3");
 $title = $metatitle[array_rand($metatitle)];
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta property="og:description" content="<?php echo $description; ?>" />
+   <meta property="og:title" content="<?php echo $title; ?>"/>
+    <meta property="og:image" content="<?php echo $img; ?>"/>
+ </head>
+ <body>
+ </body>
+ </html>
+    
+   
+     
