d6afe4fbcc38fc5107ac6f8110ff18ff4f73a983
Blogjak <bezimeni.id@gmail.com>
2017-08-05 16:23:20 +0700

    Update files.php

--- files.php
+++ files.php
@@ -1,1 +1,16 @@
-# randomize-facebook-meta-image
+<?php
+ $metaimg = array("images/1.jpg",
+              "images/2.jpg",
+              "images/3.jpg");
+$img = $metaimg[array_rand($metaimg)];
+
+$metadescription = array("Text description 1",
+"Text description 2",
+"Text description 3");
+$description = $metadescription[array_rand($metadescription)];
+
+$metatitle = array("Text title 1",
+"Text title 2",
+"Text title 3");
+$title = $metatitle[array_rand($metatitle)];
+?>
